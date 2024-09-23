9bf93beca749023eeeedef122bf86c1080e6c64d
coding4m <coding4m@gmail.com>
2024-09-23 11:29:35 +0800

    Fix NUL character on hostname.

--- src/main/java/fastdfs/client/codec/StorageServerDecoder.java
+++ src/main/java/fastdfs/client/codec/StorageServerDecoder.java
@@ -27,7 +27,13 @@ public enum StorageServerDecoder implements Replier.Decoder<StorageServer> {
     @Override
     public StorageServer decode(ByteBuf in) {
         String group = readString(in, FDFS_GROUP_LEN);
+        if (group.indexOf('\0') > 0) {
+            group = group.substring(0, group.indexOf('\0'));
+        }
         String host = readString(in, FDFS_HOST_LEN);
+        if (host.indexOf('\0') > 0) {
+            host = host.substring(0, host.indexOf('\0'));
+        }
         int port = (int) in.readLong();
         byte idx = in.readByte();
         return new StorageServer(group, host, port, idx);
